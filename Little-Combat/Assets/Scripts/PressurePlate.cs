c729e022fe599afb7ad111308bf1fbfecd69a20a
master <97041152@st.deltion.nl>
2021-01-29 17:36:03 +0100

    leg blokje op steen voor grotere steen!
    rechter muis knop om op te pakken

---
+++ Little-Combat/Assets/Scripts/PressurePlate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour
+{
+    public Transform useFunctionFromThis;
+
+    private float cooldown = 3, nextSpawn;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (Time.time > nextSpawn)
+        {
+            if (useFunctionFromThis.GetComponent<SpawnObject>())
+            {
+                nextSpawn = cooldown + Time.time;
+                useFunctionFromThis.GetComponent<SpawnObject>().SpawnObjectIn();
+            }
+        }
+    }
+}
---
+++ Little-Combat/Assets/Scripts/SpawnObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObject : MonoBehaviour
+{
+    public Transform spawnedObject;
+
+    public void SpawnObjectIn()
+    {
+        Instantiate(spawnedObject, transform.position, transform.rotation);
+    }
+}
