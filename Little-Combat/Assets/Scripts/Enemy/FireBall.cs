a26fc83904ee8ae7025737422bf5bbe40509b61e
master <97041152@st.deltion.nl>
2021-03-02 16:20:19 +0100

    kijkdannnnnnn

--- Little-Combat/Assets/Scripts/Enemy/FireBall.cs
+++ Little-Combat/Assets/Scripts/Enemy/FireBall.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class FireBall : MonoBehaviour
 {
+    public GameObject boem;
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
         {
             other.GetComponent<PlayerHealth>().GiveDamage(1);
+            GameObject UwU = Instantiate(boem, transform.position,transform.rotation);
+            Destroy(UwU, 2);
             Destroy(gameObject);
         }
     }
