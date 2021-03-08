ca9400cfd1a720997990cf902d664137a7e4bd0f
slabt <github@slabt.com>
2021-03-08 16:47:56 +0100

    Alle UI shit ofzo

---
+++ Little-Combat/Assets/Scripts/MainMenu/Settings.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.Audio;
+
+public class Settings : MonoBehaviour
+{
+    public Slider musicSlider;
+    public Slider vfxSlider;
+
+    public TMP_Dropdown qualityDropDown;
+
+    public AudioMixer vfxMixer;
+    public AudioMixer musicMixer;
+
+    public TMP_Dropdown resolutionsDropdown;
+
+    Resolution[] resolutions;
+
+    private void Start()
+    {
+        qualityDropDown.value = QualitySettings.GetQualityLevel();
+
+        resolutions = Screen.resolutions;
+
+        resolutionsDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolution = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height + "@" + resolutions[i].refreshRate + "hz";
+            options.Add(option);
+
+            if(resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolution = i;
+            }
+        }
+
+        resolutionsDropdown.AddOptions(options);
+        resolutionsDropdown.value = currentResolution;
+        resolutionsDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetVolume(float volume)
+    {
+        vfxMixer.SetFloat("volume", vfxSlider.value);
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        musicMixer.SetFloat("volume", musicSlider.value);
+    }
+
+    public void SetFullScreen(bool fullScreen)
+    {
+        Screen.fullScreen = fullScreen;
+    }
+}
