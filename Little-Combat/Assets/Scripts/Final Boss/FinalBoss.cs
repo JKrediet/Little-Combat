102efc994f98f5dabf0025f045f0cae19b12025e
master <97041152@st.deltion.nl>
2021-03-20 17:06:47 +0100

    ow shiet, begin finalbossie

--- Little-Combat/Assets/Scripts/Final Boss/FinalBoss.cs
+++ Little-Combat/Assets/Scripts/Final Boss/FinalBoss.cs
@@ -6,6 +6,11 @@ public class FinalBoss : MonoBehaviour
 {
     public List<int> crystals;
 
+    public Animator anim;
+    public bool playerInRange;
+    public int state;
+    public Transform attackPos;
+
     private void Awake()
     {
         for(int i = 0; i < 4; i++)
@@ -14,12 +19,35 @@ public class FinalBoss : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        if(playerInRange)
+        {
+            if(state == 0)
+            {
+                playerInRange = false;
+                Attack();
+            }
+        }
+    }
+
     public void Attack()
+    {
+        state = 1;
+        anim.SetInteger("State", state);
+    }
+    public void DamageHitBox()
     {
 
     }
     public void StopAttack()
     {
-
+        state = 0;
+        anim.SetInteger("State", state);
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(attackPos.position, transform.lossyScale.z / 2);
     }
 }
