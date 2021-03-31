a2f412fbddce94c803e0ee7802fa21c34755052e
slabt <github@slabt.com>
2021-03-31 13:57:35 +0200

    Music

---
+++ Little-Combat/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    public AudioClip[] playList;
+    public AudioSource source;
+    
+    private AudioClip currentClip;
+    private int currentClipIndex;
+
+    private void Start()
+    {
+        source.clip = playList[0];
+
+        currentClip = source.clip;
+        currentClipIndex = 0;
+
+        source.Play();
+
+        StartCoroutine(ReadSongPlayback());
+    }
+
+    private IEnumerator ReadSongPlayback()
+    {
+        yield return new WaitForSeconds(currentClip.length);
+
+        source.Stop();
+
+        currentClipIndex++;
+
+        source.clip = playList[currentClipIndex];
+        currentClip = source.clip;
+
+        source.Play();
+    }
+}
