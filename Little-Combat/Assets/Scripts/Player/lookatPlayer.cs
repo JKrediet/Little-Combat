50099a7b12bdcf01f75e092807cd2c8151783c55
master <97041152@st.deltion.nl>
2021-04-01 14:44:40 +0200

    hier fady blij nu! stfu

---
+++ Little-Combat/Assets/Scripts/Player/lookatPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lookatPlayer : MonoBehaviour
+{
+    public Transform player;
+    void Start()
+    {
+        player = FindObjectOfType<PlayerMovement>().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 rot = player.position - transform.position;
+        rot.y = 0;
+        transform.rotation = Quaternion.LookRotation(rot);
+    }
+}
